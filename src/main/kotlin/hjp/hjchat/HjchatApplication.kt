97de7b5545c9982cc04e62057a25b87f913be614
PHJUNITY <45454119+PHJUNITY@users.noreply.github.com>
2024-12-12 16:43:55 +0900

    initial commit

---
+++ src/main/kotlin/hjp/hjchat/HjchatApplication.kt
@@ -0,0 +1,11 @@
+package hjp.hjchat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HjchatApplication
+
+fun main(args: Array<String>) {
+    runApplication<HjchatApplication>(*args)
+}
---
+++ src/test/kotlin/hjp/hjchat/HjchatApplicationTests.kt
@@ -0,0 +1,13 @@
+package hjp.hjchat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HjchatApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

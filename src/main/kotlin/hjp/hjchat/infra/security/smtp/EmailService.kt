1a32fe45936da2c7768c42b4640c2d60a4720f21
PHJUNITY <45454119+PHJUNITY@users.noreply.github.com>
2024-12-16 13:23:02 +0900

    :bug: 오타 수정ver2

--- src/main/kotlin/hjp/hjchat/infra/security/smtp/EmailService.kt
+++ src/main/kotlin/hjp/hjchat/infra/security/smtp/EmailService.kt
@@ -13,19 +13,18 @@ class EmailService(
         val verificationUrl = "http://localhost:8080/api/oauth/verify-email?token=$token" // 실제 서비스 도메인으로 변경
         val subject = "회원가입 이메일 인증"
         val message = """
-        안녕하세요!
+        안녕하세요! HJ CHAT 입니다.
         아래 링크를 클릭하여 이메일 인증을 완료해주세요:
         $verificationUrl
         
         링크는 1시간 동안 유효합니다.
     """.trimIndent()
 
-        // 이메일 발송 로직
         mailSender.send { mimeMessage ->
             val helper = MimeMessageHelper(mimeMessage, "UTF-8")
             helper.setTo(email)
             helper.setSubject(subject)
-            helper.setText(message, false) // `true`로 변경 시 HTML로 작성 가능
+            helper.setText(message, false)
         }
     }
 }

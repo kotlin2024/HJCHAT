88f8e36c54d45ead714201b2d6fea12a2f4b332b
PHJUNITY <45454119+PHJUNITY@users.noreply.github.com>
2024-12-17 17:20:59 +0900

    :fire: 사용하지 않는 import문 제거

--- src/main/kotlin/hjp/hjchat/domain/chat/controller/ChatController.kt
+++ src/main/kotlin/hjp/hjchat/domain/chat/controller/ChatController.kt
@@ -1,28 +1,24 @@
 package hjp.hjchat.domain.chat.controller
 
-import graphql.GraphQLContext
 import graphql.kickstart.tools.GraphQLMutationResolver
 import graphql.kickstart.tools.GraphQLQueryResolver
 import hjp.hjchat.domain.chat.dto.MessageDto
 import hjp.hjchat.domain.chat.entity.Message
 import hjp.hjchat.domain.chat.entity.toResponse
 import hjp.hjchat.domain.chat.model.MessageRepository
-import hjp.hjchat.infra.security.jwt.JwtTokenManager
 import hjp.hjchat.infra.security.jwt.UserPrincipal
 import hjp.hjchat.infra.security.ouath.model.OAuthRepository
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.core.userdetails.User
 import org.springframework.stereotype.Controller
-import kotlin.jvm.optionals.getOrNull
+
 
 @Controller
 class ChatController(
     private val messageRepository: MessageRepository,
     private val oAuthRepository: OAuthRepository,
-    private val jwtTokenManager: JwtTokenManager,
 ) : GraphQLQueryResolver, GraphQLMutationResolver {
 
     @QueryMapping
